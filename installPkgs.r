53d07557b528a977ac6a3b16b53f2bf684e80118
Ani <sharma.animesh@gmail.com>
2022-11-08 16:11:26 +0000

    fix loading of BiocManager and add plot

--- installPkgs.r
+++ installPkgs.r
@@ -3,4 +3,5 @@ dir.create(file.path(dirLocal, "rLib"), showWarnings = FALSE)
 libLocal <- paste(dirLocal,"rLib",sep="/")
 reposLib="http://cran.us.r-project.org"
 install.packages(c("infotheo","BiocManager","jsonlite"), lib = libLocal, repos = reposLib)
+library("BiocManager",lib = libLocal)
 BiocManager::install(c("BiocGenerics","GenomeInfoDb","Biobase","S4Vectors","IRanges", "GenomicRanges","MatrixGenerics", "matrixStats", "SummarizedExperiment","reshape2","limma","lionessR","igraph"),  lib = libLocal)
